8723b49e0eda6f465303c6abcc9d08eb31f56088
Poul <anrivandloup@gmail.com>
2022-12-20 14:34:02 -0500

    agregar el fromMap

--- lib/model/dish.dart
+++ lib/model/dish.dart
@@ -4,4 +4,9 @@ class Dish {
 
   Dish(this.name, this.description, this.price);
 
+  Dish.fromMap(Map map){
+    this.name= map["name"];
+    this.description = map["description"];
+    this.price = map["price"];
+  }
 }
